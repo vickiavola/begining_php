25d754f0f63c710b6d7afcee945ab6c9de857ab8
Vicki Avola <vickiavola7@gmail.com>
2022-04-18 06:38:29 +0200

    initial commit

---
+++ array_1.php
@@ -0,0 +1,13 @@
+<?php
+
+// Arreglos
+$edades = [20, 18, 40];
+
+echo "\n";
+
+// Aceder a subindice dentro de un arreglo
+$edades = [20, 18, 40];
+
+echo "Una de las edades que esta dentro es " . $edades[1] ;
+
+echo "\n";
---
+++ array_2.php
@@ -0,0 +1,16 @@
+<?php
+
+//Para hacer salto de línea utilizamos  \n
+
+echo "Hola. Estoy programando con PHP \n";
+
+//Para declarar una variable se antepone el simbolo $
+$nombre = "Vicki";
+$apellido = "Avola";
+
+//Para concatenar texto o variables utilizamos .
+ echo "No, no. Yo me llamo " . $nombre . " " . $apellido . "\n";
+
+// echo "No, no. Yo me llamo $nombre $apellido \n";
+
+echo "El resultado de 4 x 5 es " . (4 * 5) . "\n";
