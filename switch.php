8a44fb424ee69645dd2f02277d3ad4ed2ba10397
Vicki Avola <vickiavola7@gmail.com>
2022-04-19 04:35:34 +0200

    Clase 7

---
+++ switch.php
@@ -0,0 +1,40 @@
+<?php
+
+// Persona 1: 9
+// Persona 2: 4
+// Persona 3: 3
+// Persona 4: 9
+// Persona 5: 1
+
+$Persona = 1;
+
+switch( $Persona){
+
+    case 1:
+          echo "Su numero favorito es el 9";
+          break;
+    
+    case 2:
+        echo "Su numero favorito es el 4";
+        break;
+
+    case 3:
+        echo "Su numero favorito es el 3";
+        break;
+     
+     case 4:
+        echo "Su numero favorito es el 9";
+        break;
+
+    case 5:
+        echo "Su numero favorito es el 1";
+        break;
+
+    default:
+        echo "Ese michi no existe :c";
+
+}
+
+echo "\n";
+
+?>
