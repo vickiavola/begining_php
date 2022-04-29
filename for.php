5154cf6f92d7a30182ae6d63c372a13f1115649e
Vicki Avola <vickiavola7@gmail.com>
2022-04-29 13:28:38 +0200

    Clase 11

--- for.php
+++ for.php
@@ -1,5 +1,15 @@
 <?php
 
+echo "\n";
+
+for ($contador = 10; $contador > 0; $contador--) {
+     echo $contador . "\n";
+}
+
+echo "\n";
+
+
+
 for ($i = 0, $j = 0; $i < 10; $i++, $j += 2) {
     // Escribir todo nuestro código
     echo "i = $i j = $j" . "\n";
@@ -10,4 +20,11 @@ for ($i=0; $i < 10; $i++) {
 }
 
 echo "\n";
+
+for ($contador = 10; $contador > 0; $contador--) {
+     echo $contador . "\n";
+}
+
+echo "\n";
+
 ?>
